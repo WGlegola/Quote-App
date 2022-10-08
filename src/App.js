0c01174478d0d55abce99743acff053d730d2dd0
Wojciech Glegola <wglegola@gmail.com>
2022-10-08 20:22:49 +0200

    initial quote commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import { Route } from "react-router-dom";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
+import { Route, Switch } from "react-router";
+import AllQuotes from "./components/pages/AllQuotes";
+import NewQuote from "./components/pages/NewQuote";
+import QuoteDetails from "./components/pages/QuoteDetail";
+
 function App() {
   return (
-    <div>
-      <Route path="/welcome">
-        <Welcome />
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
       </Route>
-      <Route path="/products">
-        <Products />
+      <Route path="/quotes/:quoteId">
+        <QuoteDetails />
       </Route>
-    </div>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ src/components/pages/QuoteDetail.js
@@ -0,0 +1,5 @@
+const QuoteDetails = () => {
+  return <h1>QuoteDetails</h1>;
+};
+
+export default QuoteDetails;
